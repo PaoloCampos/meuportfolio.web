a225eb28578a76ffb951fdc44dd6c39e0edc2c59
Paolo Campos <85637882+PaoloCampos@users.noreply.github.com>
2023-05-11 14:30:45 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,23 @@
+document.querySelector(".hamburguer").addEventListener("click", () =>
+document.querySelector(".container").classList.toggle("show-menu")
+);
+
+var audio = document.getElementById('audio');
+var audioTocando = false;
+document.querySelector('.control').onclick = function(){
+  if(audioTocando){
+        audio.pause();
+        audioTocando = false;
+        var playButton = document.querySelector('.btPause');
+        playButton.className = "btPlay";
+        playButton.innerHTML = "<i class='fa-solid fa-volume-high'></i>Ative o som";
+    } else {
+        audio.play();
+        audio.volume = 0.2;
+        audioTocando = true;
+        var playButton = document.querySelector('.btPlay')
+        playButton.className = "btPause";
+        playButton.innerHTML = "<i class='fa-solid fa-volume-xmark'></i>Deslique o som";   
+  }
+}
+
